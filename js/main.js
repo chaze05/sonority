b3dc13f8ccd18fe8503d9cd0a6ad957c7e1ebfa0
rani <rani@spin.cw>
2021-08-06 14:57:15 +0800

    reveal change

--- js/main.js
+++ js/main.js
@@ -31,7 +31,6 @@ $(function () {
             opacity: null
         };
         ScrollReveal().reveal('.trackHeader', slideUp);
-        ScrollReveal().reveal('.trackBox',slideLeft);
         ScrollReveal().reveal('.mainContent .contentHolder .row .row', { delay: 1500 });
         ScrollReveal().reveal('.contentBox', { origin: 'bottom',distance:'150%' });
         ScrollReveal().reveal('.imgHolder', { origin:'bottom',distance:'150%' });
